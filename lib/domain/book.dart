887848831af251aec0d67bed9f284fe797466f9a
Kei Fujikawa <fujikei22funk@gmail.com>
2021-06-02 22:33:27 +0900

    最新のversionに対応

--- lib/domain/book.dart
+++ lib/domain/book.dart
@@ -3,8 +3,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Book {
   Book(DocumentSnapshot doc) {
     documentID = doc.id;
-    title = doc.data()['title'];
-    imageURL = doc.data()['imageURL'];
+
+    final data = doc.data() as Map<String, Object>;
+    title = data['title'];
+    imageURL = data['imageURL'];
   }
 
   String documentID;
